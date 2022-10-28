f5653be88de5aa73bc3088566e4db57db5017d98
mohamadsheam <mohammadsheam@gmail.com>
2022-10-28 10:19:12 +0600

    chore: update base url with heroku app url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/assignment/';
+//$config['base_url'] = 'http://localhost/assignment/';
+$config['base_url'] = 'https://task-bdgrowtech.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
